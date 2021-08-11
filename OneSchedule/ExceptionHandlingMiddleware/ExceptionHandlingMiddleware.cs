32c462edd7492dd24d89715f9956f3acbed7197a
gitrepository100 <83582865+gitrepository100@users.noreply.github.com>
2021-08-11 12:29:00 +0300

    ExceptionHandlingMiddleware modified

--- OneSchedule/ExceptionHandlingMiddleware/ExceptionHandlingMiddleware.cs
+++ OneSchedule/ExceptionHandlingMiddleware/ExceptionHandlingMiddleware.cs
@@ -36,13 +36,12 @@ namespace OneSchedule
 
         private async Task HandleExceptionAsync(HttpContext context, Exception exception)
         {
+            var errorDetails = new ErrorDetails();
+            errorDetails.StatusCode = context.Response.StatusCode;
+            errorDetails.Message = $"Internal Server Error from the custom middleware. {exception.Message}";
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-            await context.Response.WriteAsync(new ErrorDetails()
-            {
-                StatusCode = context.Response.StatusCode,
-                Message = $"Internal Server Error from the custom middleware. {exception.Message}"
-            }.ToString());
+            await context.Response.WriteAsync(errorDetails.ToString());
         }
     }
 }
