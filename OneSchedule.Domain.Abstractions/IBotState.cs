4a629a834672f89c46f5ec845547f67afa053b32
gitrepository100 <83582865+gitrepository100@users.noreply.github.com>
2021-08-12 15:13:01 +0300

    StateMachine folder aded

---
+++ OneSchedule.Domain.Abstractions/IBotState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneSchedule.Domain.Abstractions
+{
+    public interface IBotState
+    {
+        public void Handle();
+    }
+}
---
+++ OneSchedule.Domain/States/EventSetTitleState.cs
@@ -0,0 +1,19 @@
+﻿
+using OneSchedule.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OneSchedule.Domain.States
+{
+    public class EventSetTitleState
+    { 
+        public void Handle() 
+        {
+                    
+        }
+    }
+}
