ae5af4c14b24bd41f21c44d64bd4169c62ad2321
VallarDark <valet.pro999993@gmail.com>
2021-08-15 18:24:55 +0300

    add 2 new strategies

---
+++ OneSchedule.Domain.Models/Strategy/FindStrategy.cs
@@ -0,0 +1,15 @@
+﻿using OneSchedule.Attributes;
+using System;
+using System.Threading.Tasks;
+
+namespace OneSchedule.Domain.Models.Strategy
+{
+    [StrategyName("find")]
+    public class FindStrategy : IStrategy
+    {
+        public Task Execute(EventDomain eventDomain)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OneSchedule.Domain.Models/Strategy/RemoveStrategy.cs
@@ -0,0 +1,15 @@
+﻿using OneSchedule.Attributes;
+using System;
+using System.Threading.Tasks;
+
+namespace OneSchedule.Domain.Models.Strategy
+{
+    [StrategyName("remove")]
+    public class RemoveStrategy : IStrategy
+    {
+        public Task Execute(EventDomain eventDomain)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
