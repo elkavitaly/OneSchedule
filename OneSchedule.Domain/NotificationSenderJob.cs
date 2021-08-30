6e89994b81cb8ca18304e66e0716a60f8253e692
gitrepository100 <83582865+gitrepository100@users.noreply.github.com>
2021-08-30 15:44:33 +0300

    Demo2 last update

---
+++ OneSchedule.Domain/NotificationSenderJob.cs
@@ -0,0 +1,25 @@
+﻿using OneSchedule.Domain.Abstractions;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneSchedule.Domain
+{
+    public class NotificationSenderJob : IJob
+    {
+        private INotificationSender _notificationSender;
+
+        public NotificationSenderJob(INotificationSender notificationSender)
+        {
+            _notificationSender = notificationSender;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await _notificationSender.SendNotification();
+        }
+    }
+}
